d0588237159eaa0353835c6f1278d7df8bd1d0cc
Borys Boiko <borys.boiko@obreey-products.com>
2025-08-27 18:00:59 +0200

    xxx50

--- sdl_presenter/src/render/layout.rs
+++ sdl_presenter/src/render/layout.rs
@@ -1,11 +1,14 @@
 use crate::render::DEFAULT_MARGIN;
 use crate::render::renderer::Renderer;
-use dudes_in_space_api::utils::math::Rect;
+use dudes_in_space_api::utils::math::{Rect, Size};
 use dudes_in_space_api::utils::utils::Float;
 
 pub trait LayoutElement<T: sdl2::render::RenderTarget> {
     fn visible(&self) -> bool;
     fn draw(&self, renderer: &mut Renderer<T>, bounding_box: Rect<Float>);
+    fn implicit_size(&self) -> Option<Size<Float>> {
+        None
+    }
 }
 
 pub struct ColumnLayout<'a, T: sdl2::render::RenderTarget> {
@@ -101,6 +104,139 @@ impl<'a, T: sdl2::render::RenderTarget> LayoutElement<T> for RowLayout<'a, T> {
                     (i as Float * (w + margin.abs()) + x, y, w, h).into(),
                 );
                 i += 1;
+            }
+        }
+    }
+}
+
+#[derive(Default)]
+pub(crate) struct ExtRowLayoutOptions {
+    preserve_aspect_ratio: bool,
+    relative_width: Option<Float>,
+}
+
+impl ExtRowLayoutOptions {
+    pub fn preserve_aspect_ratio() -> Self {
+        Self {
+            preserve_aspect_ratio: true,
+            relative_width: None,
+        }
+    }
+}
+
+pub struct ExtRowLayout<'a, T: sdl2::render::RenderTarget> {
+    elems: Vec<(Box<dyn LayoutElement<T> + 'a>, ExtRowLayoutOptions)>,
+}
+
+impl<'a, T: sdl2::render::RenderTarget> ExtRowLayout<'a, T> {
+    pub(crate) fn new(elems: Vec<(Box<dyn LayoutElement<T> + 'a>, ExtRowLayoutOptions)>) -> Self {
+        Self { elems }
+    }
+}
+
+
+impl<'a, T: sdl2::render::RenderTarget> LayoutElement<T> for ExtRowLayout<'a, T> {
+    fn visible(&self) -> bool {
+        todo!()
+    }
+
+    fn draw(&self, renderer: &mut Renderer<T>, bounding_box: Rect<Float>) {
+        assert_eq!(self.elems.len(), 2);
+
+        let (elem0,_) = &self.elems[0];
+        let (elem1,_) = &self.elems[1];
+
+        let e0_size = elem0.implicit_size().unwrap();
+
+        let qx = bounding_box.w() / e0_size.w();
+        let qy = bounding_box.h() / e0_size.h();
+
+        let q = qx.min(qy);
+
+        let e0_new_size: Size<Float> = (
+            bounding_box.w() * q,
+            bounding_box.h() * q,
+        ).into();
+
+        let e0_bb: Rect<_> = (bounding_box.left_top(), e0_new_size).into();
+
+        let e1_bb = Rect::from_lrtb_unchecked(e0_bb.right(), bounding_box.right(), bounding_box.top(), bounding_box.bottom());
+
+        elem0.draw(renderer,e0_bb);
+        elem1.draw(renderer,e1_bb);
+    }
+}
+
+#[derive(Default)]
+pub(crate) struct ExtColumnLayoutOptions {
+    preserve_aspect_ratio: bool,
+    relative_height: Option<Float>,
+}
+
+impl ExtColumnLayoutOptions {
+    pub fn relative_height(h: Float) -> Self {
+        Self {
+            preserve_aspect_ratio: false,
+            relative_height: Some(h),
+        }
+    }
+}
+
+pub struct ExtColumnLayout<'a, T: sdl2::render::RenderTarget> {
+    elems: Vec<(Box<dyn LayoutElement<T> + 'a>, ExtColumnLayoutOptions)>,
+}
+
+impl<'a, T: sdl2::render::RenderTarget> ExtColumnLayout<'a, T> {
+    pub(crate) fn new(elems: Vec<(Box<dyn LayoutElement<T> + 'a>, ExtColumnLayoutOptions)>) -> Self {
+        Self { elems }
+    }
+}
+
+impl<'a, T: sdl2::render::RenderTarget> LayoutElement<T> for ExtColumnLayout<'a, T> {
+    fn visible(&self) -> bool {
+        todo!()
+    }
+
+    fn draw(&self, renderer: &mut Renderer<T>, bounding_box: Rect<Float>) {
+
+        let (bounding_box, margin) = bounding_box.homogeneous_mul(DEFAULT_MARGIN);
+
+        if !renderer.intersects_with_view_port(&bounding_box) {
+            return;
+        }
+
+        let count = self.elems.iter().filter(|(x,_)| x.visible()).count();
+        if count == 0 {
+            return;
+        }
+
+        let margin = margin / 2.;
+        let sum_margin = margin.abs() * (count - 1) as Float;
+
+        let x = *bounding_box.x();
+        let mut y = *bounding_box.y();
+        let w = *bounding_box.w();
+
+        let elems_with_relative_height_count = self.elems.iter().filter(|(elem,options)|elem.visible() && options.relative_height.is_some()).count();
+        let sum_relative_height = self
+            .elems
+            .iter()
+            .filter_map(|(elem,options)|if elem.visible() { options.relative_height } else {None})
+            .sum::<Float>();
+        assert!(sum_relative_height >= 0.);
+        assert!(sum_relative_height <= 1.);
+
+        let rest_relative_height = (1. - sum_relative_height) / (self.elems.len() - elems_with_relative_height_count) as Float;
+
+        for (elem, options) in &self.elems {
+            if elem.visible() {
+                let h = options.relative_height.unwrap_or(rest_relative_height) * bounding_box.h();
+
+                elem.draw(
+                    renderer,
+                    (x, y, w, h).into(),
+                );
+                y += h;
             }
         }
     }
--- sdl_presenter/src/render/render_models/person_render_model.rs
+++ sdl_presenter/src/render/render_models/person_render_model.rs
@@ -1,59 +1,151 @@
-use crate::render::{RenderError, Renderer};
+use crate::render::{ExtColumnLayout, ExtColumnLayoutOptions, ExtRowLayout, ExtRowLayoutOptions, LayoutElement, RenderError, Renderer};
 use dudes_in_space_api::person::Person;
 use dudes_in_space_api::utils::color::Color;
-use dudes_in_space_api::utils::math::{Point, Rect};
+use dudes_in_space_api::utils::math::{Point, Rect, Size};
 use dudes_in_space_api::utils::utils::Float;
 use std::convert::Into;
 use std::ops::Deref;
 use std::sync::LazyLock;
 
-fn draw_little_man<T: sdl2::render::RenderTarget>(
-    renderer: &mut Renderer<T>,
-    bounding_box: Rect<Float>,
-) {
-    static POINTS: LazyLock<[Point<Float>; 16]> = LazyLock::new(|| {
-        [
-            (0., 0.).into(),
-            (-2., 1.).into(),
-            (-1., 4.).into(),
-            (-3., 5.).into(),
-            (-4., 8.).into(),
-            (-2., 6.).into(),
-            (-2., 10.).into(),
-            (-3., 18.).into(),
-            (0., 12.).into(),
-            (3., 18.).into(),
-            (2., 10.).into(),
-            (2., 6.).into(),
-            (4., 8.).into(),
-            (3., 5.).into(),
-            (1., 4.).into(),
-            (2., 1.).into(),
-        ]
-    });
+struct DrawLittleMan {
+    points: &'static [Point<Float>],
+    aabb: Rect<Float>
+}
 
-    let aabb: Rect<Float> = Rect::aabb_from_points(POINTS.deref().iter().cloned()).unwrap();
-
-    let qx = bounding_box.w() / aabb.w();
-    let qy = bounding_box.h() / aabb.h();
-
-    let q = qx.min(qy);
-
-    let points: Vec<_> = POINTS
-        .iter()
-        .map(|x| bounding_box.left_top() + (*x - aabb.left_top()) * q)
-        .collect();
-
-    renderer.draw_polygon(&points, Color::black());
-
-    for p in points {
-        renderer.fill_circle(
-            p,
-            bounding_box.w().min(*bounding_box.h()) / 50.,
-            Color::black(),
-        );
+impl DrawLittleMan {
+    fn new() -> Self {
+        static POINTS: LazyLock<[Point<Float>; 16]> = LazyLock::new(|| {
+            [
+                (0., 0.).into(),
+                (-2., 1.).into(),
+                (-1., 4.).into(),
+                (-3., 5.).into(),
+                (-4., 8.).into(),
+                (-2., 6.).into(),
+                (-2., 10.).into(),
+                (-3., 18.).into(),
+                (0., 12.).into(),
+                (3., 18.).into(),
+                (2., 10.).into(),
+                (2., 6.).into(),
+                (4., 8.).into(),
+                (3., 5.).into(),
+                (1., 4.).into(),
+                (2., 1.).into(),
+            ]
+        });
+        Self {
+            points: POINTS.deref(),
+            aabb:Rect::aabb_from_points(POINTS.deref().iter().cloned()).unwrap(),
+        }
     }
 }
+
+impl<T: sdl2::render::RenderTarget> LayoutElement<T> for DrawLittleMan {
+    fn visible(&self) -> bool {
+        todo!()
+    }
+
+    fn draw(&self, renderer: &mut Renderer<T>, bounding_box: Rect<Float>) {
+
+        let qx = bounding_box.w() / self.aabb.w();
+        let qy = bounding_box.h() / self.aabb.h();
+
+        let q = qx.min(qy);
+
+        let points: Vec<_> = self.points
+            .iter()
+            .map(|x| bounding_box.left_top() + (*x - self.aabb.left_top()) * q)
+            .collect();
+
+        renderer.draw_polygon(&points, Color::black());
+
+        for p in points {
+            renderer.fill_circle(
+                p,
+                bounding_box.w().min(*bounding_box.h()) / 50.,
+                Color::black(),
+            );
+        }
+    }
+
+    fn implicit_size(&self) -> Option<Size<Float>> {
+        todo!()
+    }
+}
+
+struct DrawLog {
+    
+}
+
+impl DrawLog {
+    fn new() -> Self {
+        Self {}
+    }
+}
+
+impl<T: sdl2::render::RenderTarget> LayoutElement<T> for DrawLog {
+    fn visible(&self) -> bool {
+        todo!()
+    }
+
+    fn draw(&self, renderer: &mut Renderer<T>, bounding_box: Rect<Float>) {
+        todo!()
+    }
+}
+
+
+
+
+
+
+struct DrawHeader {
+
+}
+
+impl DrawHeader {
+    fn new() -> Self {
+        Self {}
+    }
+}
+
+impl<T: sdl2::render::RenderTarget> LayoutElement<T> for DrawHeader {
+    fn visible(&self) -> bool {
+        todo!()
+    }
+
+    fn draw(&self, renderer: &mut Renderer<T>, bounding_box: Rect<Float>) {
+        todo!()
+    }
+}
+
+
+
+
+
+
+struct DrawFooter {
+
+}
+
+impl DrawFooter {
+    fn new() -> Self {
+        Self {}
+    }
+}
+
+impl<T: sdl2::render::RenderTarget> LayoutElement<T> for DrawFooter {
+    fn visible(&self) -> bool {
+        todo!()
+    }
+
+    fn draw(&self, renderer: &mut Renderer<T>, bounding_box: Rect<Float>) {
+        todo!()
+    }
+}
+
+
+
 
 pub struct PersonRenderModel {}
 
@@ -68,7 +160,20 @@ impl PersonRenderModel {
         person: &Person,
         bounding_box: Rect<Float>,
     ) -> Result<(), RenderError> {
-        draw_little_man(renderer, bounding_box);
+        
+        let row = ExtRowLayout::new(vec![
+            (Box::new(DrawLittleMan::new()), ExtRowLayoutOptions::preserve_aspect_ratio()),
+            (Box::new(DrawLog::new()), Default::default()),
+        ]);
+        
+        let column = ExtColumnLayout::new(vec![
+            (Box::new(DrawFooter::new()), Default::default()),
+            (Box::new(row), ExtColumnLayoutOptions::relative_height(0.5)),
+            (Box::new(DrawHeader::new()), Default::default()),
+        ]);
+
+        column.draw(renderer, bounding_box);
+        
         Ok(())
     }
 }
